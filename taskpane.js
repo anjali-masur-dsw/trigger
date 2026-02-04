dcfc68bb4dce858b0c2ba496b611fff0bb6ea735
amdsw <anjali.masur@datasciencewizards.ai>
2026-02-04 14:19:12 +0530

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -41,6 +41,7 @@ async function triggerFlowAndLoadForm() {
         const response = await fetch(flowUrl, {
             method: "POST",
             headers: {
+                'ngrok-skip-browser-warning': 'true',
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(emailData)
